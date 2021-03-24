fcccbb6aa5edb9c5b5b5f29faadf84a47fac5efa
Michael Vines <mvines@gmail.com>
2021-03-24 14:05:33 -0700

    Update to use fetch-release.sh

--- stake-o-matic.sh
+++ stake-o-matic.sh
@@ -2,18 +2,7 @@
 #
 # Downloads and runs the latest stake-o-matic binary
 #
-set -e
+set -ex
 
-solana_version=edge
-curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.0.0/install/solana-install-init.sh \
-    | sh -s - $solana_version \
-        --no-modify-path \
-        --data-dir ./solana-install \
-        --config ./solana-install/config.yml
-
-PATH="$(realpath "$PWD"/solana-install/releases/"$solana_version"*/solana-release/bin/):$PATH"
-echo PATH="$PATH"
-
-set -x
-solana --version
-exec solana-stake-o-matic "$@"
+"$(dirname "$0")"/fetch-release.sh
+exec ./solana-stake-o-matic "$@"
