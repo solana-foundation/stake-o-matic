54a953528615263cc1a292f652c78d7fa56aefa8
Solana Maintainers <maintainers@solana.foundation>
2021-05-02 22:02:02 -0700

    Add db plumbing

--- stake-o-matic.sh
+++ stake-o-matic.sh
@@ -5,4 +5,23 @@
 set -ex
 
 "$(dirname "$0")"/fetch-release.sh "$STAKE_O_MATIC_RELEASE"
-exec ./solana-stake-o-matic "$@"
+
+
+if [[ $BUILDKITE = true ]]; then
+  if [[ ! -d db ]]; then
+    git clone git@github.com:solana-labs/stake-o-matic.wiki.git db
+  fi
+  git config --global user.email maintainers@solana.foundation
+  git config --global user.name "Solana Maintainers"
+fi
+
+./solana-stake-o-matic "$@"
+
+if [[ $BUILDKITE = true ]]; then
+  cd db
+  git add *
+  if ! git diff-index --quiet HEAD; then
+    git commit -m "Automated update by $BUILDKITE_BUILD_ID"
+    git push origin
+  fi
+fi
