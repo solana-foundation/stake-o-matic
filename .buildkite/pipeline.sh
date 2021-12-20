ca8e2386193a1577bd25efab65c374a642724c84
Jon Cinque <jon.cinque@gmail.com>
2021-12-20 20:45:40 +0100

    Bump out stake-o-matic-other timeout to 180

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -13,6 +13,6 @@ while IFS=, read -r RESERVE_ACCOUNT_ADDRESS STAKE_AUTHORITY_KEYPAIR; do
   echo "    env:"
   echo "      RESERVE_ACCOUNT_ADDRESS: \"${RESERVE_ACCOUNT_ADDRESS}\""
   echo "      STAKE_AUTHORITY_KEYPAIR: \"${STAKE_AUTHORITY_KEYPAIR}\""
-  echo "    timeout: 120"
+  echo "    timeout: 180"
   group=$(( group + 1 ))
 done<"${STAKE_SOURCE_CSV:?}"
