88cf18fab0bff7a47d7963791e072572bb9a17c8
Alex Bundy <abundy@opuslogica.com>
2022-05-26 14:40:29 -0700

    Vagueify

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Encrypts the file used to list validators who don't have to meet the minimum self-stake requirement.
+# Encrypts the file.
 # Keys should be RSA. Put additional public keys in /pubkeys and add as an -inkey argument here
 
 openssl rsautl -encrypt -pubin -inkey pubkeys/ab.pub -inkey pubkeys/listkey.pub -in exclude.yml -out assets/exclude.yml.enc
