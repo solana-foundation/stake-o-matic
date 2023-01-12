98a280bc11c64117a61462c0dcd97555fece924a
tigarcia <tim.m.garcia@gmail.com>
2023-01-12 12:45:56 -0800

    Removing TPU client for RPC send test

--- bot/src/rpc_client_utils.rs
+++ bot/src/rpc_client_utils.rs
@@ -132,8 +132,9 @@ pub fn send_and_confirm_transactions_with_spinner(
                 for (index, (_i, transaction)) in pending_transactions.values().enumerate() {
                     let method = if dry_run {
                         "DRY RUN"
-                    } else if tpu_client.send_transaction(transaction) {
-                        "TPU"
+                    // TODO: UNCOMMENT CODE AFTER RPC SEND TEST
+                    //} else if tpu_client.send_transaction(transaction) {
+                    //    "TPU"
                     } else {
                         let _ = rpc_client.send_transaction_with_config(
                             transaction,
