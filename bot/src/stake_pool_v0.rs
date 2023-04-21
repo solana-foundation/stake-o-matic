ef035f7b261813f56aa600570af1d496f23a7fb1
Alex Bundy <abundy@opuslogica.com>
2023-04-20 17:15:05 -0700

    Bot: Ignore errors when distributing stake
    This should allow the bot to "finish" when some transactions don't go
    through. Running the bot subsequent times should re-try the failed
    transactions.

--- bot/src/stake_pool_v0.rs
+++ bot/src/stake_pool_v0.rs
@@ -808,16 +808,31 @@ where
         Sol(reserve_stake_balance)
     );
 
-    let ok = if dry_run {
-        true
+    let num_transaction_errors = if dry_run {
+        0
     } else {
-        !send_and_confirm_transactions_with_spinner(client, false, transactions, authorized_staker)?
-            .iter()
-            .any(|err| err.is_some())
+        match send_and_confirm_transactions_with_spinner(
+            client,
+            false,
+            transactions,
+            authorized_staker,
+        ) {
+            Ok(errors) => errors.iter().filter(|err| err.is_some()).count(),
+            Err(e) => {
+                error!("Sending transactions failed: {:?}", e);
+                return Ok((activating_total, deactivating_total));
+            }
+        }
     };
 
-    if !ok {
-        Err("One or more transactions failed to execute".into())
+    if num_transaction_errors > 0 {
+        error!(
+            "{:?} transactions failed to execute due to errors.",
+            num_transaction_errors
+        );
+        // Err("One or more transactions failed to execute".into())
+        // for now, ignore errors while distributing stake. The next time the bot runs it should retry all failed transactions.
+        Ok((activating_total, deactivating_total))
     } else {
         Ok((activating_total, deactivating_total))
     }
