fc4cedc7207391ae44a42a2403bd215ab9d94764
raykast <me@ryan-s.net>
2024-03-26 22:27:02 -0700

    Replace all cargo targets w/ deprecation warnings.

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,12 +1,1 @@
-mod entrypoint;
-pub mod instruction;
-pub mod processor;
-pub mod state;
-
-pub use solana_program;
-
-solana_program::declare_id!("reg8X1V65CSdmrtEjMgnXZk96b9SUSQrJ8n1rP1ZMg7");
-
-pub mod admin {
-    solana_program::declare_id!("AdminzUFhXiGmLZBCBeFQT5ZjQkZsHc2rUh28egNnMwd");
-}
+#![deprecated = "solana-foundation-delegation-program-registry"]
